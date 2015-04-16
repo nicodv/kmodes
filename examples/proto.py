b6771c9f8fe398564d4c7be3e525a7dbb88960ec
nico <njdevos@gmail.com>
2015-04-15 20:44:36 -0700

    updated examples and moved to new dir

---
+++ examples/proto.py
@@ -0,0 +1,15 @@
+
+import numpy as np
+from kmodes import kmodes
+
+# reproduce results on small soybean data set
+syms = np.genfromtxt('proto.csv', dtype=str, delimiter=',')[:, 0]
+xnum = np.genfromtxt('proto.csv', dtype=float, delimiter=',')[:, 1]
+xnum = np.atleast_2d(xnum).T
+xcat = np.genfromtxt('proto.csv', dtype=str, delimiter=',')[:, 2:]
+
+kproto = kmodes.KPrototypes(n_clusters=4, init='Cao', verbose=2)
+clusters = kproto.fit_predict([xnum, xcat])
+
+for s, c in zip(syms, clusters):
+    print("Symbol: {}, cluster:{}".format(s, c[0]))
---
+++ examples/soybean.py
@@ -0,0 +1,25 @@
+
+import numpy as np
+from kmodes import kmodes
+
+# reproduce results on small soybean data set
+x = np.genfromtxt('soybean.csv', dtype=int, delimiter=',')[:, :-1]
+y = np.genfromtxt('soybean.csv', dtype=str, delimiter=',', usecols=35)
+
+kmodes_huang = kmodes.KModes(n_clusters=4, init='Huang', verbose=0)
+kmodes_huang.fit_predict(x)
+kmodes_cao = kmodes.KModes(n_clusters=4, init='Cao', verbose=1)
+kmodes_cao.fit_predict(x)
+
+for result in (kmodes_huang, kmodes_cao):
+    classtable = np.zeros((4, 4), dtype=int)
+    for ii, _ in enumerate(y):
+        classtable[int(y[ii][-1]) - 1, result.labels_[ii][0]] += 1
+
+    print("\n")
+    print("    | Cl. 1 | Cl. 2 | Cl. 3 | Cl. 4 |")
+    print("----|-------|-------|-------|-------|")
+    for ii in range(4):
+        prargs = tuple([ii + 1] + list(classtable[ii, :]))
+        print(" D{0} |    {1:>2} |    {2:>2} |    {3:>2} |    {4:>2} |"
+              .format(*prargs))
