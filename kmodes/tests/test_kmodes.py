43cbd5931c64e8c755d0cda00e480c38f6f5442c
nicodv <nico.devos@autogrid.com>
2015-04-28 15:01:15 -0700

    added first proper test

--- kmodes/tests/test_kmodes.py
+++ kmodes/tests/test_kmodes.py
@@ -1,5 +1,8 @@
 
 from nose.tools import assert_equal
+from kmodes.kmodes import get_max_value_key
+
 
 def test_mode_from_dict():
-    assert_equal(1, 1)
+    max_key = get_max_value_key({'a': 3, 'b': 10, 'c': -1, 'd': 9.9})
+    assert_equal('b', max_key)
