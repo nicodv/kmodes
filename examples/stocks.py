d23ee9212af88749f07c0bb5539481df7d6e4fec
nico <njdevos@gmail.com>
2015-04-27 21:45:33 -0700

    updated documentation; rename example

--- examples/stocks.py
+++ examples/stocks.py
@@ -3,10 +3,10 @@ import numpy as np
 from kmodes import kprototypes
 
 # stocks with their market caps, sectors and countries
-syms = np.genfromtxt('proto.csv', dtype=str, delimiter=',')[:, 0]
-xnum = np.genfromtxt('proto.csv', dtype=float, delimiter=',')[:, 1]
+syms = np.genfromtxt('stocks.csv', dtype=str, delimiter=',')[:, 0]
+xnum = np.genfromtxt('stocks.csv', dtype=float, delimiter=',')[:, 1]
 xnum = np.atleast_2d(xnum).T
-xcat = np.genfromtxt('proto.csv', dtype=str, delimiter=',')[:, 2:]
+xcat = np.genfromtxt('stocks.csv', dtype=str, delimiter=',')[:, 2:]
 
 kproto = kprototypes.KPrototypes(n_clusters=4, init='Cao', verbose=2)
 clusters = kproto.fit_predict([xnum, xcat])
