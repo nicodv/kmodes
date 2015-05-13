605de72a9b7a44f80440e1495b39f0414e05e1c3
nicodv <nico.devos@autogrid.com>
2015-05-13 10:51:31 -0700

    __version__ bugfix

--- setup.py
+++ setup.py
@@ -1,10 +1,17 @@
 
-from setuptools import setup
-import sys
 import os
+import sys
+from setuptools import setup
+from distutils.util import convert_path
+
 sys.path.insert(0, os.path.join('.', 'kmodes'))
-from kmodes import __version__
+
+main_ns = {}
+ver_path = convert_path('kmodes/version.py')
+with open(ver_path) as ver_file:
+    exec(ver_file.read(), main_ns)
+
 setup(
     name='kmodes',
-    version=__version__
+    version=main_ns['__version__']
 )
