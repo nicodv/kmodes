3b6548b4f27e3c4604aaebc034a5d9dd82f83882
nico <njdevos@gmail.com>
2015-10-23 18:21:32 -0700

    added pickle tests

--- kmodes/tests/test_kmodes.py
+++ kmodes/tests/test_kmodes.py
@@ -1,8 +1,20 @@
+"""
+Tests for k-modes clustering algorithm
+"""
 
-from nose.tools import assert_equal
-from kmodes.kmodes import get_max_value_key
+import pickle
+
+from sklearn.utils.testing import assert_equal
+
+from kmodes.kmodes import KModes, get_max_value_key
 
 
 def test_mode_from_dict():
     max_key = get_max_value_key({'a': 3, 'b': 10, 'c': -1, 'd': 9.9})
     assert_equal('b', max_key)
+
+
+def test_pickle():
+    obj = KModes()
+    s = pickle.dumps(obj)
+    assert_equal(type(pickle.loads(s)), obj.__class__)
--- kmodes/tests/test_kprototypes.py
+++ kmodes/tests/test_kprototypes.py
@@ -1,1 +1,15 @@
-# TODO
+"""
+Tests for k-prototypes clustering algorithm
+"""
+
+import pickle
+
+from sklearn.utils.testing import assert_equal
+
+from kmodes.kprototypes import KPrototypes
+
+
+def test_pickle():
+    obj = KPrototypes()
+    s = pickle.dumps(obj)
+    assert_equal(type(pickle.loads(s)), obj.__class__)
