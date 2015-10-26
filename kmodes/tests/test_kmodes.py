7839b964b307e289146e1285e6655a497e7fc3a5
nico <njdevos@gmail.com>
2015-10-26 15:27:46 -0700

    added tests

--- kmodes/tests/test_kmodes.py
+++ kmodes/tests/test_kmodes.py
@@ -4,14 +4,29 @@ Tests for k-modes clustering algorithm
 
 import pickle
 
-from sklearn.utils.testing import assert_equal
+import numpy as np
+from sklearn.utils.testing import assert_equal, assert_raises, assert_array_equal
 
-from kmodes.kmodes import KModes, get_max_value_key
+from kmodes.kmodes import get_max_value_key, matching_dissim, KModes
 
 
-def test_mode_from_dict():
+def test_get_max_value_key():
     max_key = get_max_value_key({'a': 3, 'b': 10, 'c': -1, 'd': 9.9})
     assert_equal('b', max_key)
+
+
+def test_matching_dissim():
+    a = np.array([0, 1, 2, 0, 1, 2])
+    b = np.array([0, 1, 2, 0, 1, 0])
+    assert_raises(AssertionError, matching_dissim, a, b)
+
+    a = np.atleast_2d(a)
+    b = np.atleast_2d(b)
+    assert_equal(1, matching_dissim(a, b))
+
+    a = np.array([['a', 'b', 'c', 'd']])
+    b = np.array([['a', 'b', 'c', 'd'], ['d', 'c', 'b', 'a']])
+    assert_array_equal((0, 4), matching_dissim(a, b))
 
 
 def test_pickle():
