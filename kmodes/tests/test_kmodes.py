62d0eef2d4a49dc6acbbd286af7ee5e7b5714161
nicodv <nico.devos@autogrid.com>
2016-03-02 11:09:13 -0800

    improved testing

--- kmodes/tests/test_kmodes.py
+++ kmodes/tests/test_kmodes.py
@@ -60,6 +60,8 @@ SOYBEAN = np.array([
 [0,1,1,1,1,2,1,2,1,0,1,1,0,2,2,0,0,0,1,1,2,2,0,1,0,0,0,3,4,0,0,0,0,0,1,'D4'],
 [0,1,2,1,0,3,1,1,0,2,1,1,0,2,2,0,0,0,1,0,1,2,0,0,0,0,0,3,4,0,0,0,0,0,1,'D4'],
 ])
+# Drop target column
+SOYBEAN = SOYBEAN[:, :35]
 
 
 class TestKModes(unittest.TestCase):
@@ -90,6 +92,7 @@ class TestKModes(unittest.TestCase):
                              0, 0, 0, 0, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 2, 1,
                              2, 1, 2, 2, 1, 1, 2, 2, 1, 1, 2, 2, 1, 2, 1])
         np.testing.assert_array_equal(result, expected)
+        self.assertTrue(result.dtype == np.dtype(np.uint8))
 
     def test_kmodes_cao_soybean(self):
         kmodes_cao = kmodes.KModes(n_clusters=4, init='Cao', verbose=2)
@@ -98,10 +101,54 @@ class TestKModes(unittest.TestCase):
                              1, 1, 1, 1, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 0, 0,
                              0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
         np.testing.assert_array_equal(result, expected)
+        self.assertTrue(result.dtype == np.dtype(np.uint8))
 
     def test_kmodes_random_soybean(self):
         kmodes_random = kmodes.KModes(n_clusters=4, init='random', verbose=2)
         result = kmodes_random.fit(SOYBEAN)
+        self.assertIsInstance(result, kmodes.KModes)
+
+    def test_kmodes_init_soybean(self):
+        init_vals = np.array(
+            [[0, 1, 2, 1, 0, 3, 1, 1, 0, 2, 1, 1, 0, 2, 2, 0, 0, 0, 1, 0, 1, 2,
+              0, 0, 0, 0, 0, 3, 4, 0, 0, 0, 0, 0, 1],
+             [4, 0, 0, 1, 1, 1, 3, 1, 1, 1, 1, 1, 0, 2, 2, 0, 0, 0, 1, 1, 0, 3,
+              0, 0, 0, 2, 1, 0, 4, 0, 0, 0, 0, 0, 0],
+             [3, 0, 2, 1, 0, 2, 0, 2, 1, 1, 1, 1, 0, 2, 2, 0, 0, 0, 1, 0, 3, 0,
+              1, 1, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0],
+             [3, 0, 2, 0, 1, 3, 1, 2, 0, 1, 1, 0, 0, 2, 2, 0, 0, 0, 1, 1, 1, 1,
+              0, 1, 1, 0, 0, 3, 4, 0, 0, 0, 0, 0, 0]])
+        kmodes_init = kmodes.KModes(n_clusters=4, init=init_vals, verbose=2)
+        result = kmodes_init.fit_predict(SOYBEAN)
+        expected = np.array([2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1,
+                             1, 1, 1, 1, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 0, 0,
+                             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+        np.testing.assert_array_equal(result, expected)
+
+        # 5 initial centroids, 4 n_clusters
+        init_vals = np.array(
+            [[0, 1],
+             [4, 0],
+             [4, 0],
+             [3, 0],
+             [3, 0]])
+        kmodes_init = kmodes.KModes(n_clusters=4, init=init_vals, verbose=2)
+        with self.assertRaises(AssertionError):
+            kmodes_init.fit(SOYBEAN)
+
+    def test_kmodes_empty_cluster_soybean(self):
+        # Check if the clustering does not crash in case of an empty cluster.
+        init_vals = np.array(
+            [[0, 1, 2, 1, 0, 3, 1, 1, 0, 2, 1, 1, 0, 2, 2, 0, 0, 0, 1, 0, 1, 2,
+              0, 0, 0, 0, 0, 3, 4, 0, 0, 0, 0, 0, 1],
+             [4, 0, 0, 1, 1, 1, 3, 1, 1, 1, 1, 1, 0, 2, 2, 0, 0, 0, 1, 1, 0, 3,
+              0, 0, 0, 2, 1, 0, 4, 0, 0, 0, 0, 0, 0],
+             [3, 0, 2, 1, 0, 2, 0, 2, 1, 1, 1, 1, 0, 2, 2, 0, 0, 0, 1, 0, 3, 0,
+              1, 1, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0],
+             [3, 0, 2, 0, 1, 3, 1, 2, 0, 1, 1, 0, 0, 2, 2, 0, 0, 0, 1, 1, 1, 1,
+              0, 1, 1, 0, 0, 3, 4, 0, 0, 0, 0, 0, 0]])
+        kmodes_init = kmodes.KModes(n_clusters=4, init=init_vals, verbose=2)
+        result = kmodes_init.fit(SOYBEAN)
         self.assertIsInstance(result, kmodes.KModes)
 
 
--- kmodes/tests/test_kprototypes.py
+++ kmodes/tests/test_kprototypes.py
@@ -33,12 +33,21 @@ class TestKProtoTypes(unittest.TestCase):
         s = pickle.dumps(obj)
         assert_equal(type(pickle.loads(s)), obj.__class__)
 
+    def test_kprotoypes_stocks(self):
+        # Number/index of categoricals does not make sense
+        kproto = kprototypes.KPrototypes(n_clusters=4, init='Cao', verbose=2)
+        with self.assertRaises(AssertionError):
+            kproto.fit_predict(STOCKS, categorical=[1, 3])
+        with self.assertRaises(AssertionError):
+            kproto.fit_predict(STOCKS, categorical=[0, 1, 2])
+
     def test_kprotoypes_huang_stocks(self):
         np.random.seed(42)
-        kproto_huang = kprototypes.KPrototypes(n_clusters=4, init='Huang', verbose=2)
+        kproto_huang = kprototypes.KPrototypes(n_clusters=4, n_init=1, init='Huang', verbose=2)
         result = kproto_huang.fit_predict(STOCKS, categorical=[1, 2])
-        expected = np.array([3, 0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2])
+        expected = np.array([0, 3, 3, 3, 3, 2, 2, 2, 2, 1, 1, 1])
         np.testing.assert_array_equal(result, expected)
+        self.assertTrue(result.dtype == np.dtype(np.uint8))
 
     def test_kprotoypes_cao_stocks(self):
         np.random.seed(42)
@@ -46,11 +55,45 @@ class TestKProtoTypes(unittest.TestCase):
         result = kproto_cao.fit_predict(STOCKS, categorical=[1, 2])
         expected = np.array([2, 3, 3, 3, 3, 0, 0, 0, 0, 1, 1, 1])
         np.testing.assert_array_equal(result, expected)
+        self.assertTrue(result.dtype == np.dtype(np.uint8))
 
     def test_kprotoypes_random_stocks(self):
         kproto_random = kprototypes.KPrototypes(n_clusters=4, init='random', verbose=2)
         result = kproto_random.fit(STOCKS, categorical=[1, 2])
         self.assertIsInstance(result, kprototypes.KPrototypes)
+
+    def test_kprotoypes_init_stocks(self):
+        # Wrong order
+        init_vals = [
+            np.array([[6, 2],
+                      [5, 2],
+                      [4, 2],
+                      [3, 2]]),
+            np.array([[382.27919457],
+                      [350.76963718],
+                      [13.31595618],
+                      [540.50533708]])
+        ]
+        kproto_init = kprototypes.KPrototypes(n_clusters=4, init=init_vals, verbose=2)
+        with self.assertRaises(AssertionError):
+            kproto_init.fit_predict(STOCKS, categorical=[1, 2])
+
+        init_vals = [
+            np.array([[0.],
+                      [0.],
+                      [0.],
+                      [0.]]),
+            np.array([[6, 2],
+                      [5, 2],
+                      [4, 2],
+                      [3, 2]])
+        ]
+        np.random.seed(42)
+        kproto_init = kprototypes.KPrototypes(n_clusters=4, init=init_vals, verbose=2)
+        result = kproto_init.fit_predict(STOCKS, categorical=[1, 2])
+        expected = np.array([0, 2, 2, 2, 2, 3, 3, 3, 3, 1, 1, 1])
+        np.testing.assert_array_equal(result, expected)
+        self.assertTrue(result.dtype == np.dtype(np.uint8))
 
 
 if __name__ == '__main__':
