2deb4678e5cf5d8be21404889fdf45816774e0ed
Nico de Vos <njdevos@gmail.com>
2016-09-26 17:37:59 -0700

    Updated dependencies

--- setup.py
+++ setup.py
@@ -21,9 +21,9 @@ setup(
     description=DESCRIPTION,
     long_description=open('README.rst', 'r').read(),
     install_requires=[
-        'numpy>=1.10.4',
-        'scikit-learn>=0.17.1',
-        'scipy>=0.17.0',
+        'numpy==1.11.1',
+        'scikit-learn==0.17.1',
+        'scipy==0.18.1',
     ],
     classifiers=['Development Status :: 3 - Alpha',
                  'Intended Audience :: Science/Research',
