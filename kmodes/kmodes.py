Bugfix for invoking fit then predicting on new data. Need to keep track of initial feature vector for attribute frequency calculation in ng_diss. Although this means that the encoded FV will not be freed by the GC! Alternative is to build a map of attribute frequencies at the beginning, but this adds unnecessary memory overhead if not predicting new data (e.g., just invoking fit or fit_predict). Therefore, I believe that maintaining a reference to the encoded FV is the best choice.

kmodes/kmodes.py
@@ -116,7 +116,7 @@ def move_point_cat(point, ipoint, to_clust, from_clust, cl_attr_freq,
     return cl_attr_freq, membship, centroids
 
 
-def _labels_cost(X, centroids, dissim, membship):
+def _labels_cost(X, centroids, dissim, membship, Xinit=None):
     """Calculate labels and cost function given a matrix of points and
     a list of centroids for the k-modes algorithm.
     """
@@ -127,7 +127,7 @@ def _labels_cost(X, centroids, dissim, membship):
     cost = 0.
     labels = np.empty(npoints, dtype=np.uint8)
     for ipoint, curpoint in enumerate(X):
-        diss = dissim(centroids, curpoint, X=X, membship=membship)
+        diss = dissim(centroids, curpoint, X=(X if Xinit is None else Xinit), membship=membship)
         clust = np.argmin(diss)
         labels[ipoint] = clust
         cost += diss[clust]
@@ -272,7 +272,7 @@ def k_modes(X, n_clusters, max_iter, dissim, init, n_init, verbose):
         print("Best run was number {}".format(best + 1))
 
     return all_centroids[best], enc_map, all_labels[best], \
-        all_costs[best], all_n_iters[best]
+        all_costs[best], all_n_iters[best], X
 
 
 class KModes(BaseEstimator, ClusterMixin):
@@ -359,7 +359,7 @@ class KModes(BaseEstimator, ClusterMixin):
         """
 
         self._enc_cluster_centroids, self._enc_map, self.labels_,\
-            self.cost_, self.n_iter_ = k_modes(X,
+            self.cost_, self.n_iter_, self.X = k_modes(X,
                                                self.n_clusters,
                                                self.max_iter,
                                                self.cat_dissim,
@@ -392,7 +392,7 @@ class KModes(BaseEstimator, ClusterMixin):
         assert hasattr(self, '_enc_cluster_centroids'), "Model not yet fitted."
         X = check_array(X, dtype=None)
         X, _ = encode_features(X, enc_map=self._enc_map)
-        return _labels_cost(X, self._enc_cluster_centroids, self.cat_dissim, genMembshipArray(self.labels_))[0]
+        return _labels_cost(X, self._enc_cluster_centroids, self.cat_dissim, genMembshipArray(self.labels_), self.X)[0]
 
     @property
     def cluster_centroids_(self):

kmodes/kprototypes.py
@@ -50,7 +50,7 @@ def _split_num_cat(X, categorical):
     return Xnum, Xcat
 
 
-def _labels_cost(Xnum, Xcat, centroids, num_dissim, cat_dissim, gamma, membship):
+def _labels_cost(Xnum, Xcat, centroids, num_dissim, cat_dissim, gamma, membship, XcatInit=None):
     """Calculate labels and cost function given a matrix of points and
     a list of centroids for the k-prototypes algorithm.
     """
@@ -63,7 +63,7 @@ def _labels_cost(Xnum, Xcat, centroids, num_dissim, cat_dissim, gamma, membship)
     for ipoint in range(npoints):
         # Numerical cost = sum of Euclidean distances
         num_costs = num_dissim(centroids[0], Xnum[ipoint])
-        cat_costs = cat_dissim(centroids[1], Xcat[ipoint], X=Xcat, membship=membship)
+        cat_costs = cat_dissim(centroids[1], Xcat[ipoint], X=(Xcat if XcatInit is None else XcatInit), membship=membship)
         # Gamma relates the categorical cost to the numerical cost.
         tot_costs = num_costs + gamma * cat_costs
         clust = np.argmin(tot_costs)
@@ -308,7 +308,7 @@ def k_prototypes(X, categorical, n_clusters, max_iter, num_dissim, cat_dissim,
 
     # Note: return gamma in case it was automatically determined.
     return all_centroids[best], enc_map, all_labels[best], \
-        all_costs[best], all_n_iters[best], gamma
+        all_costs[best], all_n_iters[best], gamma, Xcat
 
 
 class KPrototypes(kmodes.KModes):
@@ -404,7 +404,7 @@ class KPrototypes(kmodes.KModes):
         # If self.gamma is None, gamma will be automatically determined from
         # the data. The function below returns its value.
         self._enc_cluster_centroids, self._enc_map, self.labels_, self.cost_,\
-            self.n_iter_, self.gamma = k_prototypes(X,
+            self.n_iter_, self.gamma, self.Xcat = k_prototypes(X,
                                                     categorical,
                                                     self.n_clusters,
                                                     self.max_iter,
@@ -436,7 +436,7 @@ class KPrototypes(kmodes.KModes):
         Xnum, Xcat = check_array(Xnum), check_array(Xcat, dtype=None)
         Xcat, _ = encode_features(Xcat, enc_map=self._enc_map)
         return _labels_cost(Xnum, Xcat, self._enc_cluster_centroids,
-                            self.num_dissim, self.cat_dissim, self.gamma, genMembshipArray(self.labels_))[0]
+                            self.num_dissim, self.cat_dissim, self.gamma, genMembshipArray(self.labels_), self.Xcat)[0]
 
     @property
     def cluster_centroids_(self):