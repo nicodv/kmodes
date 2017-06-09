637cbc72a76af883178ce29441b3b93b96025623
nicodv <nico.devos@autogrid.com>
2017-06-08 22:30:35 -0700

    clean up utils

--- kmodes/util/dissim.py
+++ kmodes/util/dissim.py
@@ -2,10 +2,8 @@
 Dissimilarity measures for clustering
 """
 
-# Author: 'Nico de Vos' <njdevos@gmail.com>
-# License: MIT
-
 import numpy as np
+
 
 def matching_dissim(a, b, **kwargs):
     """Simple matching dissimilarity function"""
@@ -19,26 +17,33 @@ def euclidean_dissim(a, b, **kwargs):
     return np.sum((a - b) ** 2, axis=1)
 
 
-# Implementation of Ng et al.'s dissimilarity measure
-# 	Michael K. Ng, Mark Junjie Li, Joshua Zhexue Huang, and Zengyou He, "On the
-# 	Impact of Dissimilarity Measure in k-Modes Clustering Algorithm", IEEE
-# 	Transactions on Pattern Analysis and Machine Intelligence, Vol. 29, No. 3,
-# 	January, 2007
-# Author: 'Ben Andow' <beandow@ncsu.edu>
-# Note that membship must be a rectangular array such that the len(membship) = len(a) and len(membship[i]) = X.shape[1]
 def ng_dissim(a, b, X, membship):
-    """Ng et al.'s dissimilarity measure"""
+    """Ng et al.'s dissimilarity measure, as presented in
+    Michael K. Ng, Mark Junjie Li, Joshua Zhexue Huang, and Zengyou He, "On the
+    Impact of Dissimilarity Measure in k-Modes Clustering Algorithm", IEEE
+    Transactions on Pattern Analysis and Machine Intelligence, Vol. 29, No. 3,
+    January, 2007
+
+    Note that membship must be a rectangular array such that the
+    len(membship) = len(a) and len(membship[i]) = X.shape[1]
+    """
+    def calcCJR(b, X, memj, idr):
+        """Num objects w/ category value x_{i,r} for rth attr in jth cluster"""
+        xcids = np.where(np.in1d(memj.ravel(), [1]).reshape(memj.shape))
+        return float((np.take(X, xcids, axis=0)[0][:, idr] == b[idr]).sum(0))
+
+    def calc_dissim(b, X, memj, idr):
+        # Size of jth cluster
+        CJ = float(np.sum(memj))
+        return (1.0 - (calcCJR(b, X, memj, idr) / CJ)) if CJ != 0.0 else 0.0
+
     if len(membship) != len(a) and len(membship[0]) != X.shape[1]:
-        raise ValueError("'membship' must be a rectangular array where the number of rows in 'membship' equals the number of rows in 'a' and the number of columns in 'membship' equals the number of rows in 'X'.")
+        raise ValueError("'membship' must be a rectangular array where "
+                         "the number of rows in 'membship' equals the "
+                         "number of rows in 'a' and the number of "
+                         "columns in 'membship' equals the number of rows in 'X'.")
 
-    return np.array([ np.array([calc_dissim(b, X, membship[idj], idr, idj) if b[idr] == t else 1.0 for idr,t in enumerate(val_a) ]).sum(0) for idj,val_a in enumerate(a) ])
-
-# Num objects w/ category value x_{i,r} for rth attr in jth cluster
-def calcCJR(b, X, memj, idr):
-    xcids = np.where(np.in1d(memj.ravel(), [1]).reshape(memj.shape))
-    return float((np.take(X, xcids, axis=0)[0][:, idr] == b[idr]).sum(0))
-
-def calc_dissim(b, X, memj, idr, idj):
-    CJ = float(np.sum(memj))           # Size of jth cluster
-    return (1.0 - (calcCJR(b, X, memj, idr) / CJ)) if CJ != 0.0 else 0.0
-
+    return np.array([np.array([calc_dissim(b, X, membship[idj], idr, idj)
+                               if b[idr] == t else 1.0
+                               for idr, t in enumerate(val_a)]).sum(0)
+                     for idj, val_a in enumerate(a)])
