407f85e59f4d7505800564dd8a63ce55ced1e001
Nico de Vos <njdevos@gmail.com>
2018-11-28 18:36:30 -0800

    Drop Python 2 support

--- setup.py
+++ setup.py
@@ -34,8 +34,6 @@ setup(
                  'Operating System :: Unix',
                  'Operating System :: MacOS',
                  'Programming Language :: Python',
-                 'Programming Language :: Python :: 2',
-                 'Programming Language :: Python :: 2.7',
                  'Programming Language :: Python :: 3',
                  'Programming Language :: Python :: 3.4',
                  'Programming Language :: Python :: 3.5',
