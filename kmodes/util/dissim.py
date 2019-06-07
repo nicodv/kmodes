305ef37b0fb444deeb8af13f7e4f735b1f27da85
jamacanpenguin <6195012+jamacanpenguin@users.noreply.github.com>
2019-06-07 15:18:32 +0100

    Update dissim.py

--- kmodes/util/dissim.py
+++ kmodes/util/dissim.py
@@ -63,6 +63,6 @@ def jaccard_dissim(a, b, **_):
     i = np.sum((a == 1) & (b == 1), axis=1)
     j = np.sum((a == 1) | (b == 1), axis=1)
     if j.any != 0:
-        return (( j - i ) / j )
+        return ((j - i) / j)
     else:
         return 1
