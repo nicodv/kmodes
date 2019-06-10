0c4dbd4c0fc268f68e8c945122e0dc05c1f5f78d
jamacanpenguin <6195012+jamacanpenguin@users.noreply.github.com>
2019-06-10 12:30:41 +0100

    Update test_dissim.py

--- kmodes/util/tests/test_dissim.py
+++ kmodes/util/tests/test_dissim.py
@@ -104,14 +104,14 @@ class TestDissimilarityMeasures(unittest.TestCase):
         assert_array_equal(np.array([mdiss_12, mdiss_22]),
                            ng_dissim(centroids, X[2], X=X, membship=membship))
 
-    def test_jaccard__binary_dissim(self):
+    #def test_jaccard__binary_dissim(self):
         #a = np.array([[1, 0, 0, 1, 1, 0]])
         #b = np.array([[1, 1, 1, 0, 1, 1]])
         #assert_equal(2/3, jaccard_binary_dissim(a, b))
 
-        a = np.array([[0, 0, 1, 1, 0, 0]])
-        b = np.array([[0, 1, 1, 0, 0, 1]])
-        assert_equal(0.75, jaccard_binary_dissim(a, b))
+        #a = np.array([[0, 0, 1, 1, 0, 0]])
+        #b = np.array([[0, 1, 1, 0, 0, 1]])
+        #assert_equal(0.75, jaccard_binary_dissim(a, b))
         
         #a = np.array([[0, 0, 0, 0, 0, 0]])
         #b = np.array([[0, 0, 0, 0, 0, 0]])
